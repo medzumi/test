2a5eb1261b3cbd8e334063a3205917721a60508b
Pavel Kudrin <pavel.k@notfoundgames.com>
2022-04-22 03:27:44 +0300

    presenters fix

--- Assets/Game/CoreLogic/Money/MoneyComponent.cs
+++ Assets/Game/CoreLogic/Money/MoneyComponent.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using ApplicationScripts.CodeExtensions;
 using ApplicationScripts.Ecs;
 using ApplicationScripts.Ecs.Utility;
 using ApplicationScripts.Logic.Config;
 using EcsViewModelPresenting;
 using Leopotam.EcsLite;
 using Utilities.GenericPatterns;
+using Utilities.Pooling;
+using ViewModel;
 
 namespace Game.CoreLogic
 {
@@ -20,6 +23,91 @@ namespace Game.CoreLogic
     public interface ILinkableComponent
     {
         IEnumerable<int> GetLinks();
+    }
+    
+    [Serializable]
+    public class MoneyPresenter : AbstractEcsPresenter<MoneyPresenter, MoneyComponent>
+    {
+        public IViewModelProperty<int> MoneyReactiveProperty;
+
+        public override void Initialize(EcsPresenterData ecsPresenterData)
+        {
+            MoneyReactiveProperty = ecsPresenterData.ViewModel.GetViewModelData<IViewModelProperty<int>>("Money");
+            base.Initialize(ecsPresenterData);
+        }
+
+        public override void Update(MoneyComponent data)
+        {
+            MoneyReactiveProperty.SetValue(data.Value);
+        }
+    }
+
+    public interface IEcsPresenter : IDisposable, IClonable<IEcsPresenter>
+    {
+        public void Initialize(EcsPresenterData ecsPresenterData);
+    }
+    
+    public interface IEcsPresenter<TData> : IDisposable, IEcsPresenter
+    {
+        public void Update(TData data);
+    }
+
+    public struct EcsPresenterData
+    {
+        public EcsWorld ModelWorld;
+        public int ModelEntity;
+        public IViewModel ViewModel;
+    }
+    
+    public abstract class PoolableObject<T> : IDisposable where T : PoolableObject<T>, new()
+    {
+        private static readonly Pool<T> _pool;
+
+        private bool _isDisposed = false;
+        
+        static PoolableObject()
+        {
+            _pool = new Pool<T>(0, () => new T());
+        }
+
+        protected PoolableObject()
+        {
+            
+        }
+
+        public static T Create()
+        {
+            var obj = _pool.Get();
+            obj._isDisposed = false;
+            return obj;
+        }
+
+        private readonly List<IDisposable> _disposables = new List<IDisposable>();
+
+        public IDisposable AddTo(IDisposable disposable)
+        {
+            _disposables.Add(disposable);
+            return disposable;
+        }
+        
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                foreach (var disposable in _disposables)
+                {
+                    disposable?.Dispose();
+                }    
+                _disposables.Clear();
+                DisposeHandler();
+                _isDisposed = true;
+                _pool.Release(this as T);
+            }
+        }
+
+        protected virtual void DisposeHandler()
+        {
+        }
     }
 
     public class PresenterResolver
@@ -36,13 +124,6 @@ namespace Game.CoreLogic
     {
         private readonly List<string> _binders = new List<string>();
         private readonly List<IDisposable> _disposables = new List<IDisposable>();
-        private int _viewModelEntity = -1;
-
-        public override void Initialize(EcsWorld world, int entity, EcsWorld viewModelWorld, int viewModelEntity)
-        {
-            base.Initialize(world, entity,viewModelWorld, viewModelEntity);
-            _viewModelEntity = viewModelEntity;
-        }
 
         public override void Update(TData data)
         {
@@ -56,7 +137,7 @@ namespace Game.CoreLogic
             foreach (var entity in data.GetLinks())
             {
                 var presenter = Singletone<PresenterResolver>.instance.Resolve<TData>(_binders[index]);
-                presenter.Initialize(EcsWorld, entity, EcsWorld, _viewModelEntity);
+                presenter.Initialize(EcsPresenterData);
                 _disposables[index] = presenter;
             }
         }
@@ -83,5 +164,116 @@ namespace Game.CoreLogic
         }
 
         public string ComponentName => $"LinkPresenter<{typeof(TData).Name}>";
+    }
+
+    public abstract class AbstractEcsPresenter<TPresenter> : PoolableObject<TPresenter>, IEcsPresenter
+        where TPresenter : AbstractEcsPresenter<TPresenter>, new()
+    {
+        protected EcsPresenterData EcsPresenterData { get; private set; }
+        protected EcsPool<DisposeComponent> DisposeComponentPool { get; private set; }
+        
+        public virtual void Initialize(EcsPresenterData ecsPresenterData)
+        {
+            EcsPresenterData = ecsPresenterData;
+            DisposeComponentPool = ecsPresenterData.ModelWorld.GetPool<DisposeComponent>();
+            DisposeComponentPool.Ensure(ecsPresenterData.ModelEntity).Disposables.Add(this);
+            ecsPresenterData.ViewModel.AddTo(this);
+        }
+        
+        
+        public IEcsPresenter Clone()
+        {
+            return CloneHandler();
+        }
+
+        protected virtual TPresenter CloneHandler()
+        {
+            return AbstractEcsPresenter<TPresenter>.Create();
+        }
+    }
+
+
+    public struct DisposeComponent : IEcsAutoReset<DisposeComponent>
+    {
+        public List<IDisposable> Disposables;
+
+        public void AutoReset(ref DisposeComponent c)
+        {
+            if (c.Disposables.IsNull())
+            {
+                c.Disposables = new List<IDisposable>();
+            }
+            else
+            {
+                foreach (var disposable in c.Disposables)
+                {
+                    disposable?.Dispose();
+                }
+                c.Disposables.Clear();
+            }
+        }
+    }
+
+        public abstract class AbstractEcsPresenter<TPresenter, TData> : AbstractEcsPresenter<TPresenter>, IEcsPresenter<TData>
+        where TPresenter : AbstractEcsPresenter<TPresenter, TData>, new()
+    {
+        protected EcsPool<ListComponent<IEcsPresenter<TData>>> _presentersPool;
+
+        public override void Initialize(EcsPresenterData ecsPresenterData)
+        {
+            base.Initialize(ecsPresenterData);
+            _presentersPool = ecsPresenterData.ModelWorld.GetPool<ListComponent<IEcsPresenter<TData>>>();
+            _presentersPool.Ensure(ecsPresenterData.ModelEntity).ComponentData.Add(this);
+        }
+
+        public virtual void Update(TData data)
+        {
+        }
+
+        protected override void DisposeHandler()
+        {
+            base.DisposeHandler();
+            var data = _presentersPool.Read(EcsPresenterData.ModelEntity).ComponentData;
+            data.Remove(this);
+            if (data.Count == 0)
+            {
+                _presentersPool.Del(EcsPresenterData.ModelEntity);
+            }
+        }
+    }
+
+    public sealed class AggregatePresenter : AbstractEcsPresenter<AggregatePresenter>
+    {
+        private List<IEcsPresenter> _presenters = new List<IEcsPresenter>();
+
+        public override void Initialize(EcsPresenterData ecsPresenterData)
+        {
+            base.Initialize(ecsPresenterData);
+            foreach (var ecsPresenter in _presenters)
+            {
+                ecsPresenter.Initialize(ecsPresenterData);
+            }
+        }
+
+        protected override void DisposeHandler()
+        {
+            base.DisposeHandler();
+            foreach (var ecsPresenter in _presenters)
+            {
+                ecsPresenter.Dispose();
+            }
+            _presenters.Clear();
+        }
+
+        protected override AggregatePresenter CloneHandler()
+        {
+            var clone = base.CloneHandler();
+            foreach (var ecsPresenter in _presenters)
+            {
+                clone._presenters.Add(ecsPresenter.Clone());
+            }
+
+            return clone;
+        }
     }
 }
--- Assets/Test/MoneyPresenter.cs
+++ Assets/Test/MoneyPresenter.cs
@@ -13,77 +13,7 @@ using ViewModel;
 
 namespace EcsViewModelPresenting
 {
-    [Serializable]
-    public class MoneyPresenter : AbstractEcsPresenter<MoneyPresenter, MoneyComponent>
-    {
-        [Bind("HardValue/Money")] 
-        public IViewModelProperty<int> MoneyReactiveProperty;
-
-        public override void Initialize(EcsWorld ecsWorld, int currentEntity, EcsWorld viewModelWorld, int viewModelEntity)
-        {
-            base.Initialize(ecsWorld, Entity, viewModelWorld, viewModelEntity);
-        }
-
-        public override void Update(MoneyComponent data)
-        {
-            MoneyReactiveProperty.SetValue(data.Value);
-        }
-
-        public override string ToString()
-        {
-            return nameof(MoneyPresenter);
-        }
-    }
-
-    public interface IEcsPresenter<TData> : IDisposable, IClonable<IEcsPresenter<TData>>
-    {
-        public void Initialize(EcsWorld world, int entity, EcsWorld viewModelWorld, int viewModelEntity);
-
-        public void Update(TData data);
-    }
     
-    public abstract class Presenter<T> : IBindData, IDisposable where T : Presenter<T>, new()
-    {
-        private static readonly Pool<T> _pool;
 
-        private bool _isDisposed = false;
-        
-        static Presenter()
-        {
-            _pool = new Pool<T>(0, () => new T());
-        }
-
-        protected Presenter()
-        {
-            
-        }
-
-        public static T Create()
-        {
-            var obj = _pool.Get();
-            obj._isDisposed = false;
-            return obj;
-        }
-
-        private readonly List<IDisposable> _disposables = new List<IDisposable>();
-
-        public void Dispose()
-        {
-            if (!_isDisposed)
-            {
-                foreach (var disposable in _disposables)
-                {
-                    disposable?.Dispose();
-                }    
-                _disposables.Clear();
-                DisposeHandler();
-                _isDisposed = true;
-                _pool.Release(this as T);
-            }
-        }
-
-        protected virtual void DisposeHandler()
-        {
-        }
-    }
+   
 }
