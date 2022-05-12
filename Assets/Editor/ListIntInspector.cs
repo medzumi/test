dac6c36a919a7d3a73b2567b07f16f6efe443251
medzumi <pkkudrin@gmail.com>
2022-05-13 00:28:56 +0300

    save changes

---
+++ Assets/Editor/ListIntInspector.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Game.CoreLogic;
+using Leopotam.EcsLite.UnityEditor;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace Editor
+{
+    public class ContainerComponentInspector : EcsComponentInspectorTyped<ContainerComponent>
+    {
+        private class Inspector
+        {
+            private readonly ReorderableList _reorderableList;
+            public readonly List<int> _list;
+            public bool IsDirty { get; private set; } = false;
+
+            public Inspector(List<int> list)
+            {
+                _list = list;
+                _reorderableList = new ReorderableList(_list, typeof(int));
+                _reorderableList.elementHeightCallback = index =>
+                {
+                    return EditorGUI.GetPropertyHeight(SerializedPropertyType.Integer, GUIContent.none);
+                };
+                _reorderableList.drawElementCallback = (rect, index, active, focused) =>
+                {
+                    var previousElement = _list[index];
+                    _list[index] = EditorGUI.IntField(rect, _list[index]);
+                    if (previousElement != _list[index])
+                        IsDirty = true;
+                };
+                _reorderableList.onChangedCallback = reorderableList =>
+                {
+                    IsDirty = true;
+                };
+            }
+
+            public bool OnGui()
+            {
+                _reorderableList.DoLayoutList();
+                var isDirty = IsDirty;
+                IsDirty = false;
+                return isDirty;
+            }
+        }
+        
+        private readonly Dictionary<int, Inspector> _reorderableLists = new Dictionary<int, Inspector>();
+
+        public override bool OnGuiTyped(string label, ref ContainerComponent value, EcsEntityDebugView entityView)
+        {
+            if(!_reorderableLists.TryGetValue(entityView.Entity, out var inspector) || !ReferenceEquals(inspector._list, value.List))
+            {
+                _reorderableLists[entityView.Entity] = inspector = new Inspector(value.List);
+            }
+
+            return inspector.OnGui();
+        }
+    }
+}
--- Assets/Game/PresenterLogic/NamePresenter.cs
+++ Assets/Game/PresenterLogic/NamePresenter.cs
@@ -6,6 +6,27 @@ namespace Game.PresenterLogic
 {
     public class NamePresenter : AbstractPresenter<NamePresenter, IViewModel, NameComponent>
     {
-        
+        public string PropertyKey;
+
+        private IViewModelProperty<string> _nameProperty;
+
+        public override void Initialize(EcsPresenterData ecsPresenterData, IViewModel view)
+        {
+            base.Initialize(ecsPresenterData, view);
+            _nameProperty = view.GetViewModelData<IViewModelProperty<string>>(PropertyKey);
+        }
+
+        protected override void Update(NameComponent data)
+        {
+            base.Update(data);
+            _nameProperty.SetValue(data.Value);
+        }
+
+        protected override NamePresenter CloneHandler()
+        {
+            var clone = base.CloneHandler();
+            clone.PropertyKey = PropertyKey;
+            return clone;
+        }
     }
 }
